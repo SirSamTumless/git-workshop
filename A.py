87a88c285874446d1bba8040f0ffa086dceffd02
Sam Thomas <sam@Sams-MacBook-Pro.local>
2019-07-11 15:08:31 +0100

    Text Toto

--- A.py
+++ A.py
@@ -1,1 +1,33 @@
-print("Hey let's print")
+print("sssssoo++:          -//+++++++oyhddmmmmmmmNNNNNNNNNNNNNNNNNNNNmmmhyo/:---------------.....``..````..
+sssssoo++/.         .://+++++++yhdmmmdhhdmmddddmddmNNmmmNNNmmmmddhhs+:-::-------------......````..:/
+sosssooo+/:         `://///++shmmNmdsoydddhhhhhhhhmmddddmmmmdddhddyys/:::::::::::::::----..``...:+/+
+ssssssoo++/.         -:////+sydmNNh++oyyhsyyyyyyyyddhdhhhdmdhyyhddhs+:-::::::::::::::::-......:++/:.
+ssssssooo+/:`        .://///osdNNh///osyoosssssssyhhyyyyyyhhhsoshhdh+--:::::::::::::::-....-/++/-...
+sssssssoo++/-        `-://///+dNd+///++o++oooossssyysssssyyyys+++ohms--:::-:::::::::-....-/o+/-.....
+ssssoossoo++/.        -://////dmy////+++++++ooooooosoooooooso+////sdh+::-::::::::::----:+oo/-......`
+sssssosssoo++:`       .://////ymo///+ooosssssooooooooooooooooo///:+mdo--::::::::-----:oss/-.......:o
+ssssssosoooo+/-       .://////oms/ooyyhdmmmmdhyssssyyhddmddhyyo//-+myo:::::::::----/oss/-...-:/:+yhs
+ooooooo+++++//:--.....-::://///dmdmmdddmdmmmmdddhhddmmmmmmmmmdddy:sds::::::::----/sso/----:+oo+/+ss+
+ysysssssooooo+/:-------::::::::yNNyydmhmNNmmmmNmmdmNNmmmmNNmdmddNNNyo::::::----/oso/---:+yysso+/--/:
+yysyyssssooooo+/:---::::/++++/:oydssyhhdmmmmmdNhsshNdmmmmmmdhhyyhy+y/-:::----/os+::::/shhhyyys+/-.`:
+yyyyyyssssosoo+/::--:::://////:+sohossyyhhhhyhdo+oodhyyhhhhyyoooy::so.------::/-::::yhdddhhyyso/:.`-
+yyyyysyssssooo++/:--:::://////:/+/oyoosssssshdo+/++odyooossssooy/.-:.-.--/o+:----../hhddddhhyys+:-``
+yyyyyyyyysssooo+/::::::://////:////+sssyyyyhho++oo++shdhysssyyy+:`-...-/o/:---..:sdhyyhhhdhhyys+:-` 
+yyyyyysssssssoo+//::::::://///::+++++++++osysyhsssshysyhyyys+++/:.:.-/+/-...../yhys/.osyyhhhyys+:-` 
+hhhyyyyysssssoo+/::--..--:::::::/so++/+++osyhmmhyyhdmhhyyssoo+/+////+:...``:shy++.-: -+osyhhyys+/-. 
+hhhhyyyyyyyyssoo+/:-     .:::::-.syoo+++oosydmmmmmddddhyysoo+o++so/:..``.+yyo/-`..`-  /+osyyysso/:-`
+hhhhhhyyyyysssoo++/:.    `-:::..+syso++oyhddmNNNmmmmmmmddhyo+oooys/```/shs+:.`   ` -  -/+oyyyyso/:-`
+yyyyyyyyyyysssso++/:-`    -::..-/syyssyddmmmmmdhddhddddmmmdhyo+shyo-.sy:/-```    ` `` `:/+syyyso+:-`
+syyhhyyyyyyyyssoo+//::----::....-o:oyysyhhdddddddddddddddhhssyyddys+``--..       `  `  :/+osyyso+:-.
+/yhhhhhhhhyyyyssoo+////://:.....-/:/ydhddhyyyyyhhddhhyyyhhdhhhdmhsso-. ` -        ` `  ./+osyyso+/-.
+hhhdhhhhhhhhyyyssoo+//::-:-.....-:++smmmNmmdhdmmNNNmddddmmmmmNNmhyso:.`  .        ` `  `/++osyso+/:+
+ddddhyyhhhhhhhhy+:-...-.`.`.....--:+sdmmNNNmmdmmmmmddmmNNNNmdNNhyso+:-o:.`..``    `  - ./+oosyyso++h
+ddds-  -ohhhs+:..-:-./:..s/-....---:/ymmmmNNmmdmmmmmmmNNNNddNNdyso+/::yy+--:/:-..``  .  /+oossyysoyd
+dddddhsoshs:.--.-:+-:+-..yhho:----..-+hNNmdmmNNNNNNNNNNmmddNNmysso++//yhs+/oso/-::-.````-+oosyyhyyhm
+hddddddy+:---::.-:o--:..:yhdmdo/:::-:/shmmNmdhhhhddddhhddmNNmyyy++oy+oshhyssys+//+/:-..-.`-+syhhhhdm
+/hddy+-../o/:::--/+---:+/oyydddho+////ssydhdmmmdddddddmmmNNdhsoo+oys+yohdhhyyyo++oo+/-.-:-`.-ohhhddm
+ho:.....:so/::/-:o--://::/s+ydddhho++oo+shhhhhhdmNmmmmNNNdyyyo+oo+//yyoyhhhhhyso+ooss+-.::-::-:shddm
+-.-:::-:+y+/:///+/-:::-:/+++oydhyhysoso+oyyssyyssydNNNNhsosyoo+os++shsoyhhhhhyysooosys/--::oo:--.-om
+--//////+yoo/++oo--://os+::/ohhhyyysooysoyysooo++osymdooosyh+++/ooshhs+yyhhhhhyssoossys+///ss:---...
+-:/+////+sss+oos/:/+++o+//:/osossyyso+oo+yhyyso+++oysoosyyyy//ooosyhhs/syyhhhhhysoossyso+++yy+:----.
+::+o++//+syyydhs:/++ooso/::/+so+ossso+++/soosooooosooossysys/:ossyyyys++syyhhdhysoossssso++hy++:-:/:")
